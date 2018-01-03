30fa2b72d7d0f69da9e906764857780a3778dba7
Pablo Ruiz <pabloruiz55@gmail.com>
2018-01-03 08:15:53 -0300

    Fixed as per comments from previous commit

--- contracts/PolyDistribution.sol
+++ contracts/PolyDistribution.sol
@@ -20,13 +20,11 @@ contract PolyDistribution is Ownable {
 
   uint256 public AVAILABLE_TOTAL_SUPPLY       = 1000000000 * decimals;
 
-  uint256 public AVAILABLE_PRESALE_SUPPLY     = 2625000000 * decimals; // 100% Release Jan 24th 2018
+  uint256 public AVAILABLE_PRESALE_SUPPLY     =  262500000 * decimals; // 100% Release Jan 24th 2018
   uint256 public AVAILABLE_FOUNDER_SUPPLY     =  150000000 * decimals; // 25% Release Jan 24th, 2019 + 25% release yearly after
-  uint256 public AVAILABLE_AIRDROP_SUPPLY     =  100000000 * decimals; // 10% Released Jan 24th, 2019 + 10% monthly after
+  uint256 public AVAILABLE_AIRDROP_SUPPLY     =   10000000 * decimals; // 10% Released Jan 24th, 2019 + 10% monthly after
   uint256 public AVAILABLE_ADVISOR_SUPPLY     =   15000000 * decimals;  // 100% Released on August 24th, 2018
   uint256 public AVAILABLE_RESERVE_SUPPLY     =  562500000 * decimals; // 10M Released every month after
-
-  // IMPORTANT! The amounts above don't match and need to be reviewed
 
   uint256 public grandTotalAllocated = 0;
   uint256 public grandTotalClaimed = 0;
@@ -35,7 +33,7 @@ contract PolyDistribution is Ownable {
   // Allocation with vesting information
   struct Allocation {
     uint8 AllocationSupply; // Type of allocation
-    uint256 cliffDuration;  // Tokens are locked until
+    uint256 endCliff;       // Tokens are locked until
     uint256 endVesting;     // This is when the tokens are fully unvested
     uint256 totalAllocated; // Total tokens allocated
     uint256 amountClaimed;  // Total tokens claimed
@@ -47,10 +45,12 @@ contract PolyDistribution is Ownable {
 
   /**
     * @dev Constructor function - Set the poly token address
+    * @param _startTime is the time when PolyDistribution goes live
     */
-  function PolyDistribution() public {
+  function PolyDistribution(uint _startTime) public {
+    require(_startTime >= now);
     require(AVAILABLE_TOTAL_SUPPLY == AVAILABLE_PRESALE_SUPPLY.add(AVAILABLE_FOUNDER_SUPPLY).add(AVAILABLE_AIRDROP_SUPPLY).add(AVAILABLE_ADVISOR_SUPPLY).add(AVAILABLE_RESERVE_SUPPLY));
-    startTime = now;
+    startTime = _startTime;
     POLY = new PolyToken(this);
   }
 
@@ -67,47 +67,47 @@ contract PolyDistribution is Ownable {
     require(allocations[_recipient].totalAllocated == 0);
 
     AllocationType selectedAllocationType;
-    uint cliffDuration;
+    uint endCliff;
     uint endVesting;
 
     string memory fromSupply;
     if (_supply == 0) {
       fromSupply = 'presale';
       selectedAllocationType = AllocationType.PRESALE;
-      cliffDuration = 0;
+      endCliff = 0;
       endVesting = startTime;
       AVAILABLE_PRESALE_SUPPLY = AVAILABLE_PRESALE_SUPPLY.sub(_totalAllocated);
 
     } else if (_supply == 1) {
       fromSupply = 'founder';
       selectedAllocationType = AllocationType.FOUNDER;
-      cliffDuration = startTime + 1 years;
+      endCliff = startTime + 1 years;
       endVesting = startTime + 4 years;
       AVAILABLE_FOUNDER_SUPPLY = AVAILABLE_FOUNDER_SUPPLY.sub(_totalAllocated);
 
     } else if (_supply == 2) {
       fromSupply = 'airdrop';
       selectedAllocationType = AllocationType.AIRDROP;
-      cliffDuration = 0;
+      endCliff = 0;
       endVesting = startTime + 1 years;
       AVAILABLE_AIRDROP_SUPPLY = AVAILABLE_AIRDROP_SUPPLY.sub(_totalAllocated);
 
     } else if (_supply == 3) {
       fromSupply = 'advisor';
       selectedAllocationType = AllocationType.ADVISOR;
-      cliffDuration = startTime + 215 days;
+      endCliff = startTime + 215 days;
       endVesting = startTime;
       AVAILABLE_ADVISOR_SUPPLY = AVAILABLE_ADVISOR_SUPPLY.sub(_totalAllocated);
 
     } else if (_supply == 4) {
       fromSupply = 'reserve';
-      cliffDuration = startTime + 100 days;
-      endVesting = startTime + 4 years;
       selectedAllocationType = AllocationType.RESERVE;
+      endCliff = startTime + 100 days;
+      endVesting = startTime + 4 years;
       AVAILABLE_RESERVE_SUPPLY = AVAILABLE_RESERVE_SUPPLY.sub(_totalAllocated);
     }
 
-    allocations[_recipient] = Allocation(uint8(selectedAllocationType), cliffDuration, endVesting , _totalAllocated, 0);
+    allocations[_recipient] = Allocation(uint8(selectedAllocationType), endCliff, endVesting , _totalAllocated, 0);
 
     grandTotalAllocated = grandTotalAllocated.add(_totalAllocated);
 
@@ -120,15 +120,19 @@ contract PolyDistribution is Ownable {
     * @param _totalAllocated The total amount of POLY available to the receipient (after vesting)
     */
   function updateAllocation(address _recipient, uint256 _totalAllocated) public onlyOwner {
-
+    require(now < startTime);
+    require(_recipient != address(0));
   }
 
   /**
     * @dev Transfer a recipients available allocation to their address
     */
   function transferTokens (address _recipient) public {
+    // Preven just anyone from transferring someone else's allocation
+    require(msg.sender == owner || msg.sender == _recipient);
     require(allocations[_recipient].amountClaimed < allocations[_recipient].totalAllocated);
-    require(now >= allocations[_recipient].cliffDuration);
+    require(now >= allocations[_recipient].endCliff);
+    require(now >= startTime);
 
     uint tokensToTransfer = 0;
     uint totalAllocated = allocations[_recipient].totalAllocated;
@@ -137,7 +141,7 @@ contract PolyDistribution is Ownable {
     if (now >= vestingTime)
         tokensToTransfer = totalAllocated;
     else
-        tokensToTransfer = totalAllocated.mul(now.sub(startTime)).div(vestingTime);
+        tokensToTransfer = totalAllocated.mul(now.sub(startTime)).div(vestingTime.sub(startTime));
 
     uint tokensAlreadyClaimed = allocations[_recipient].amountClaimed;
 
